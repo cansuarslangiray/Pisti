d395abc4658ad989a2986a12a24e0cb854d753bd
cansuarslangiray <cansuarslangiray8@gmail.com>
2022-12-19 02:05:05 +0300

    added some comment line

--- src/Cards.java
+++ src/Cards.java
@@ -19,39 +19,39 @@ public class Cards {
         int index =0;
         for(int i=0; i<carsType.length;i++) {
             for (int j = 1; j <= 13; j++) {
-                if (j == 1) {
+                if (j == 1) {// check if it is 1 or not because deck have A insteade of 1.
                     Cards cards1 = new Cards(carsType[i], "A", 1);
                     cards[index] = cards1;
                     index++;
                 }
-                else if (j == 2) {
+                else if (j == 2) { // check if it is 2 or not because if it is 2, card must has 2 point.
                     Cards cards1 = new Cards(carsType[i], "2", 2);
                     cards[index] = cards1;
                     index++;
                 }
-                else if (j == 10) {
+                else if (j == 10) { // check if it is 10 or not because if it is 10, card must has 10 point.
                     Cards cards1 = new Cards(carsType[i], "10", 3);
                     cards[index] = cards1;
                     index++;
                 }
-                else if (j == 11) {
+                else if (j == 11) { // check if it is 11 or not because deck have J insteade of 11.
                     Cards cards1 = new Cards(carsType[i], "J", 1);
                     cards[index] = cards1;
                     index++;
                 }
-                else if (j == 12) {
+                else if (j == 12) { // check if it is 12 or not because deck Q instade of 12.
                     Cards cards1 = new Cards(carsType[i], "Q", 1);
                     cards[index] = cards1;
                     index++;
                 }
-                else if (j == 13) {
+                else if (j == 13) { // check if it is 13 or not because deck K instade of 13.
                     Cards cards1 = new Cards(carsType[i], "K", 1);
                     cards[index] = cards1;
                     if (i != 3) {
                         index++;
                     }
                 }
-                else {
+                else { // not need to check number because all the other number have same ponit.(1)
                     Cards cards1 = new Cards(carsType[i], Integer.toString(j), 1);
                     cards[index] = cards1;
                     index++;
@@ -65,8 +65,8 @@ public class Cards {
         Random random = new Random();
         int index = oldCards.length;
         int current = 0;
-        while (current< oldCards.length/2) {
-                int newIndex = random.nextInt(0, index);
+        while (current< oldCards.length/2) { // diveded by 2 because we change random number and index. index is decreasing.
+                int newIndex = random.nextInt(0, index);// pick a random number between index and 0.
                 Cards temp = oldCards[index-1];
                 oldCards[index-1] = oldCards[newIndex];
                 oldCards[newIndex] = temp;
